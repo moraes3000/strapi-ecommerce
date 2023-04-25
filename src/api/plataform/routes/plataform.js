0963696089f0f7c1ac321605ebac0a723e80b63d
Bruno Moraes <bruno_bmoraes@hotmail.com>
2023-04-24 21:54:30 -0300

    add model  game plataform

---
+++ src/api/plataform/routes/plataform.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * plataform router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::plataform.plataform');
---
+++ src/api/plataform/services/plataform.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * plataform service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::plataform.plataform');
